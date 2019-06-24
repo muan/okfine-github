48e961bf94624f068ea6677bf8ff258b82de8b32
Mu-An Chiou <me@muanchiou.com>
2019-06-24 18:20:15 -0400

    Add stylesheet and name functions

--- okfine-github.safariextension/main.js
+++ okfine-github.safariextension/main.js
@@ -2,10 +2,18 @@ document.addEventListener('pjax:end', init)
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
+  removeHovercards()
+  unlinkBranchNames()
+}
+
+// Features
+function removeHovercards() {
   for (const el of document.querySelectorAll('[data-hovercard-url]')) {
     el.removeAttribute('data-hovercard-url')
   }
+}
 
+function unlinkBranchNames() {
   const prHeader = document.querySelector('.js-pull-header-details')
   if (prHeader) {
     for (const el of prHeader.querySelectorAll('.commit-ref > a, .head-ref > a, .base-ref > a')) {
@@ -18,6 +26,7 @@ function init() {
   }
 }
 
+// Helpers
 function unwrap(el) {
   const parent = el.parentNode
   while (el.firstChild) parent.insertBefore(el.firstChild, el)
